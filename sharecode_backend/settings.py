e2704078826fbf15e1735f3b3d7286153412794b
jameszhang01194320 <wuab@yahoo.com>
2025-08-14 16:26:37 -0700

    commit

--- sharecode_backend/settings.py
+++ sharecode_backend/settings.py
@@ -66,6 +66,7 @@ CSRF_COOKIE_SECURE = True
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',# 第三方库
     'snippets',# 自建应用
-    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -85,7 +87,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'sharecode_backend.urls'
